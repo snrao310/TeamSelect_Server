6bae7e660f070a2d624fc796ab6d21f93adca73c
Suresh Gururajan <sgr@asu.edu>
2016-04-16 13:43:02 -0700

    Updated test.js to handle post requests to the URL: http://localhost:3000/friends

--- test.js
+++ test.js
@@ -1,12 +1,21 @@
-// Required steps:
+// Required NPM modules: mongodb client, express js, body-parser
 // npm install mongodb
 // npm install express --save
+// npm install body-parser --save
 
-var express     =   require("express");
-var app         =   express();
-var dbclient    =   require('mongodb').MongoClient;
+var express     =   require("express"); // express js app
+var dbclient    =   require('mongodb').MongoClient; // mongo db client
+var bodyParser  =   require('body-parser'); // support json encoded bodies
+
 var database;
 var friend_collection;
+
+var app         =   express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Connect to mongo db
 dbclient.connect("mongodb://localhost:27017/sgrdatabase_test",
   function(err, db) {
     if (!err) {
@@ -16,20 +25,32 @@ dbclient.connect("mongodb://localhost:27017/sgrdatabase_test",
   }
 );
 
-app.get('/', function(req, res) {
+app
+.get('/', function(req, res) { // Root
   res.send('Hey There');
-});
-
-app.get('/blog', function(req, res){
+})
+.get('/blog', function(req, res){ // Blog
   res.send('You have reached my blog');
-});
-
-app.get('/friends', function(req, res) {
+})
+.get('/friends', function(req, res) { // Get all Friends
   friend_collection.find().toArray(function(err, items){
-    res.send(JSON.stringify({"friends":items}));
+    res.send(
+      JSON.stringify(
+        {"friends":items}
+      )
+    );
   });
-});
+})
+.post('/friends', function(req, res){ // Create new friend
+  var username = req.body.username;
+  var email = req.body.email;
+  var latitude = req.body.latitude;
+  var json_str = JSON.stringify({"username":username,"email":email,"latitude":latitude});
 
-app.listen(3000, function() {
+  console.log("JSON string: \n");
+  console.log(json_str);
+  res.send('Got the request to create new friend, ' + req.body.username);
+})
+.listen(3000, function() {
   console.log('Listening on port 3000');
 });
