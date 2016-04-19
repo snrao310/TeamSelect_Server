5a97523eaf270b717df0ee7aa331aab686a730b3
Suresh Gururajan <sgr@asu.edu>
2016-04-18 20:05:46 -0700

    Added functionality for user registration

--- test.js
+++ test.js
@@ -48,15 +48,54 @@ app
     );
   });
 })
-.get('/search', function(req, res) {
-  
+.get('/user/:id', function(req, res) {
+  // This code piece was added temporarily to show the
+  // team how to search for stuff with REST APIs
+  // - Suresh G
+  username = req.params.id;
+  friend_collection.find({"username":username});
+  console.log("Got get request for ID: " + req.params.id);
 })
-.post('/friends', function(req, res){ // Create new friend
+.post('/filter',function(req, res){
+  // URL Format: http://<server>:3000/filter
+  console.log(req.body.skillset + "\n");
+  console.log(req.body.aoi);
+})
+.get('/skills/:id/aoi/:id2', function(req, res) {
+  // URL Format: http://<server>:3000/skills/ruby&python&java&android
+  var skills = req.params.id;
+  var aoi = req.params.id2;
+  var skillset = sills.split("&");
+  var aoiset = oai.split("&");
+  console.log(arr.toString());
+})
+.get('/aoi/:id', function(req, res){
+  // URL Format: http://<server>:3000/aoi/mobile&software&bigdata
+
+
+  console.log(arr.toString());
+  friend_collection.find({});
+})
+.post('/register', function(req, res){ // Create new friend
   var username = req.body.username;
-  var email = req.body.email;
-  var latitude = req.body.latitude;
-  var longitude = req.body.longitude;
-  var json_str = JSON.stringify({"username":username,"email":email,"latitude":latitude,"longitude":longitude});
+  var password = req.body.password;
+  var name = req.body.name;
+  var location = req.body.location;
+  var mail = req.body.mail;
+  var phone = req.body.phone;
+  var aoi = req.body.aoi;
+  var skillset = req.body.skillset;
+  var json_str = JSON.stringify(
+    { "username":username,
+      "password":password,
+      "name":name,
+      "location":location,
+      "mail":mail,
+      "phone":phone,
+      "aoi":aoi,
+      "skillset":skillset
+    }
+  );
   friend_collection.insertOne(JSON.parse(json_str));
   console.log("Got POST request from client: " + req.connection.remoteAddress);
   console.log(json_str);
